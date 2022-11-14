bea72c47ebffe19012f14d48eb77336b3241d573
Theodore Redlon <redlontheo@gmail.com>
2022-11-14 13:03:47 -0500

    Added comments, removed redundancy

--- scraping_github_topics_repositories/topic_info.py
+++ scraping_github_topics_repositories/topic_info.py
@@ -2,67 +2,22 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-topics_url = "https://github.com/topics"
-response = requests.get(topics_url)
-
-page_contents = response.text
-
-# Writes page contents copy to local html file
-with open("webpage.html", "w", encoding="utf-8") as f:
-    f.write(page_contents)
-
-# creates a beautiful soup object, using html file and html parser
-doc = BeautifulSoup(page_contents, "html.parser")
-
-topic_title_tags = doc.find_all(
-    "p", {"class": "f3 lh-condensed mb-0 mt-1 Link--primary"}
-)
-# saves the first element's title
-# first_element_title = topic_title_tags[0].text
-# However I want to save all the titles in a list
-topic_titles = []
-for tag in topic_title_tags:
-    topic_titles.append(tag.text)
-
-
-topic_desc_tags = doc.find_all("p", {"class": "f5 color-fg-muted mb-0 mt-1"})
-# saves the first element's description
-# first_element_desc = topic_desc_tags[0].text.strip()
-# Saves all title descriptions
-topic_descs = []
-for tag in topic_desc_tags:
-    topic_descs.append(tag.text.strip())
-
-
-topic_link_tags = doc.find_all("a", {"class": "no-underline flex-grow-0"})
-# saves the first element's URL
-# first_element_link = "https://github.com" + topic_link_tags[0]["href"]
-# Saves all title URLS
-topic_links = []
-for tag in topic_link_tags:
-    topic_links.append("https://github.com" + tag["href"])
-
-# creates a ditionary of the 3 lists I made
-topics_dict = {"title": topic_titles, "description": topic_descs, "url": topic_links}
-
-# create a pandas dataframe and put dictionary of lists inside
-topics_df = pd.DataFrame(topics_dict)
-
-# transer dataframe into CSV file, index=none so that it doesn't list the index in the csv file
-topics_df.to_csv("topics.csv", index=None)
-
-
+# Searches the bs4 doc for p tags, and a specific class selector.
+# Loops through soup object containing title.
+# Returns a list of each element's text which in this case is the title.
 def get_topic_title(doc):
     topic_title_tags = doc.find_all(
         "p", {"class": "f3 lh-condensed mb-0 mt-1 Link--primary"}
     )
     topic_titles = []
     for tag in topic_title_tags:
-        topic_titles.append(tag.text)
+        topic_titles.append(tag.text.strip())
     return topic_titles
 
 
+# Searches the bs4 doc for p tags, and a specific class selector.
+# Loops through soup object containing description.
+# Returns a list of each element's text which in this case is the description of title.
 def get_topic_desc(doc):
     topic_desc_tags = doc.find_all("p", {"class": "f5 color-fg-muted mb-0 mt-1"})
     topic_descs = []
@@ -71,6 +26,10 @@ def get_topic_desc(doc):
     return topic_descs
 
 
+# Searches the bs4 doc for a tags, and a specific class selector.
+# Loops through soup object containing links/urls.
+# Returns a list of our base url (+) each element's key at tag["href"]
+# example - tag["href"] could look like /topics/bootstrap or /topics/cpp
 def get_topic_url(doc):
     topic_link_tags = doc.find_all("a", {"class": "no-underline flex-grow-0"})
     topic_links = []
@@ -79,14 +38,22 @@ def get_topic_url(doc):
     return topic_links
 
 
+# Combines all functions to return a dataframe of topic information
+# Returns a pandas dataframe
 def scrape_topics():
     topics_url = "https://github.com/topics"
     response = requests.get(topics_url)
+
+    # If the request fails then stops the program
     if response.status_code != 200:
         raise Exception("Failed to load page {}".format(topics_url))
+
+    # Creates bs4 object to be passed to get 3 pieces of information in a dictionary
+    doc = BeautifulSoup(response.text, "html.parser")
     topics_dict = {
         "title": get_topic_title(doc),
         "description": get_topic_desc(doc),
         "url": get_topic_url(doc),
     }
+    # Passes dictionary and returns a pandas dataframe
     return pd.DataFrame(topics_dict)
--- scraping_github_topics_repositories/topic_page_info.py
+++ scraping_github_topics_repositories/topic_page_info.py
@@ -18,13 +18,16 @@ def parse_star_count(star_str):
 
 
 # Returns all the required info about a repository
-# h3_tag = result of soup searching a <h3> tag
-# star_tag = result of soup searching a <span> tag
 def get_repo_info(h3_tag, star_tag):
+
+    # 2 child a tags of h3_tags containing username and repository
     a_tags = h3_tag.find_all("a")
     username = a_tags[0].text.strip()
     repo = a_tags[1].text.strip()
+
+    # Creates repositories URL
     url = base_url + a_tags[1]["href"]
+    # Gets star tag string and uses parse_star_count() to convert to correct type
     stars = parse_star_count(star_tag.text.strip())
     return username, repo, url, stars
 
@@ -44,14 +47,15 @@ def get_topic_page(topic_url):
 
 def get_topic_repos(topic_doc):
 
-    # Get h3 tag containing title, repo_url, username
+    # Searches the bs4 topic doc for all h3 tags, and a specific class selector.
     username_repo_tags = topic_doc.find_all(
         "h3", {"class": "f3 color-fg-muted text-normal lh-condensed"}
     )
-    # Get star tags
+    # Searches the bs4 topic doc for all span tags, and a specific class selector. Contains star information
     project_stars = topic_doc.find_all("span", {"class": "Counter js-social-count"})
 
-    # Get repo information
+    # Loops through the number of h3 tags in username_repo_tags
+    # Data extracted one by one using get_repo_info() passing a unique h3 tag and unique span tag
     topic_repos_dict = {"username": [], "repo_name": [], "repo_url": [], "stars": []}
     for i in range(len(username_repo_tags)):
         repo_info = get_repo_info(username_repo_tags[i], project_stars[i])
@@ -63,24 +67,34 @@ def get_topic_repos(topic_doc):
 
 
 def scrape_topic(topic_url, path):
+    # If I already have a CSV file with the same name and directory I skip
     if os.path.exists(path):
         print("The file {} already exists. Skipping...".format(path))
         return
+    # get_topic_page() is passed a url from topics.csv file and returnsa BeautifulSoup object
+    # get_topic_repos() is passed that bs4 object where it extracts the data
     topic_df = get_topic_repos(get_topic_page(topic_url))
+    # Extracted data is converted to a csv file
     topic_df.to_csv(path, index=None)
 
 
+# This is the function that will run both files
 def scrape_topics_repos():
     print("Scraping list of topics.")
+    # Uses scrape topics from topic_info.py to get dataframe of topic info
     topics_df = topic_info.scrape_topics()
+    # Create CSV of that topic info dataframe
+    topics_df.to_csv("topics.csv", index=None)
 
-    # Create Directory to put CSV files
+    # Makes a directoy/folder to put CSV files in, called "data"
     dir_name = "data"
     os.makedirs(dir_name, exist_ok=True)
 
+    # Search my topics dataframe to use scrape_topic for each element
     for index, row in topics_df.iterrows():
         print('Scraping top repositories for "{}"'.format(row["title"]))
         scrape_topic(row["url"], dir_name + "/{}.csv".format(row["title"]))
 
 
+# Runs scraper
 scrape_topics_repos()
