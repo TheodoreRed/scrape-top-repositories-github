b6911fc0c1f831b874d43cedb0ac9f377bfacdb6
Theodore Redlon <redlontheo@gmail.com>
2022-11-21 09:56:42 -0500

    added functionality to query data

--- scraping_github_topics_repositories/use_the_data.py
+++ scraping_github_topics_repositories/use_the_data.py
@@ -1,9 +1,15 @@
 import random
 import time
 import webbrowser
+import os
 
 
 data_dict = {}
+list_topic = []
+list_stars = []
+list_repos = []
+list_username = []
+list_urls = []
 
 
 def open_data_create_dict():
@@ -14,6 +20,13 @@ def open_data_create_dict():
     for x in range(len(data) - 1):
         list1 = data[x].split(",")
         data_dict[x] = list1
+
+    for x in range(len(data_dict)):
+        list_topic.append(data_dict[x][0])
+        list_stars.append(data_dict[x][1])
+        list_repos.append(data_dict[x][2])
+        list_username.append(data_dict[x][3])
+        list_urls.append(data_dict[x][4])
 
 
 def get_random_url():
@@ -30,13 +43,8 @@ def open_random_repo():
 
 
 def get_top_repo():
-    list_stars = []
-    list_urls = []
-    for x in range(len(data_dict)):
-        list_stars.append(data_dict[x][1])
-        list_urls.append(data_dict[x][4])
 
-    star_url_dict = {"star": list_stars, "url": list_urls}
+    # star_url_dict = {"star": list_stars, "url": list_urls}
     bigNum = 0
     for idx, num in enumerate(list_stars):
         if int(num) > bigNum:
@@ -54,22 +62,47 @@ def get_top_10_topics():
     pass
 
 
+def search_everything(usr_input):
+    for idx, x in enumerate(list_urls):
+        if x.__contains__(usr_input.lower()):
+            print(list_urls[idx] + " Topic: " + list_topic[idx])
+
+
+def clear_command_line():
+    os.system("cls")
+
+
+def topic_name_data():
+    print("Enter topic name:")
+    usr_input = input().upper()
+    for idx, x in enumerate(list_topic):
+        if x.upper() == usr_input:
+            print(list_urls[idx], "has", list_stars[idx], "stars")
+
+
 def use_the_data():
 
     while True:
         print(
-            "Enter: 'r' for random repo - 'open r' to open a random repo - '1' prints top repo - 'q' - to quit"
+            "Enter: 'r' for random repo - 'open r' to open a random repo - '1' prints top repo - 'by topic' follow-up search by topic name - 'clear' to clear command line - 'q' to quit"
         )
+        print("Or search for anything by name:")
         usr_input = input()
         if usr_input == "r":
             get_random_url()
-        elif usr_input == "1":
-            get_top_repo()
         elif usr_input == "open r":
             open_random_repo()
+        elif usr_input == "1":
+            get_top_repo()
+        elif usr_input == "by topic":
+            topic_name_data()
+        elif usr_input == "clear":
+            clear_command_line()
         elif usr_input == "q":
             break
-        time.sleep(3)
+        else:
+            search_everything(usr_input)
+        time.sleep(0.5)
 
 
 def main():
